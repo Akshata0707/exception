b962f9fa7745c5b8d3a9a0098a5d857e3123a780
M1064286 <M1064286@MT-PG02AGZ2>
2021-06-03 12:11:26 +0530

    savedChanges

--- src/unchecked/UncheckedExceptions.java
+++ src/unchecked/UncheckedExceptions.java
@@ -5,6 +5,7 @@ public class UncheckedExceptions {
 	public static void main(String[] args)  {
 		int a=10;
 		int b=0;
+		System.out.println("this is unchecked Exception example");
 		try {
 			System.out.println(a/b);
 		}catch(ArithmeticException e){
